3e73f3fdbc89353d88a4e5b9ca9d181af7f5e9f8
lfjrb-desk <ojuniorbitencourt@gmail.com>
2023-03-12 17:49:37 -0300

    Atualizando meu site

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
+// function playAudio() {
+//   var audio = document.getElementById("audio");
+//   if (audio.paused) {
+//     audio.play();
+//   } else {
+//     audio.pause();
+//   }
+// }
 
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('show');
+function playAudio() {
+  var audio = document.getElementById("audio");
+  var music = document.querySelector(".music");
+  var bars = document.querySelectorAll(".bar");
+  
+  if (audio.paused) {
+    audio.play();
+    music.classList.add("playing");
+    for (var i = 0; i < bars.length; i++) {
+      bars[i].style.animationPlayState = "running";
+    }
+  } else {
+    audio.pause();
+    music.classList.remove("playing");
+    for (var i = 0; i < bars.length; i++) {
+      bars[i].style.animationPlayState = "paused";
+    }
+  }
+}
+
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    menuItems.forEach(item => item.classList.remove('active'));
+    item.classList.add('active');
+  });
 });
